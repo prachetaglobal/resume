1501dbbe8d48614bafbb134c8cce32bbae5768fe
Chirag <Prachetaglobal@gmail.com>
2026-05-02 20:59:18 +0530

    db local

--- config/database.php
+++ config/database.php
@@ -2,6 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'resume_maker');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'myadmin');
+define('DB_PASS', 'StrongPassword123!');
 define('DB_CHARSET', 'utf8mb4');
